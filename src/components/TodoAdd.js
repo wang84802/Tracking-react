f1a8716c696abfad49cf60d9fe4a0103e8e545cb
wang84802 <wang84802@gmail.com>
2019-05-15 16:11:27 +0800

    first commit

---
+++ src/components/TodoAdd.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class TodoAdd extends Component {
+    constructor(props) {
+        super(props);
+        this._onAddClick = this._onAddClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref="addInput" />
+                <button onClick={this._onAddClick}>Create</button>
+            </div>
+        );
+    }
+
+    _onAddClick() {
+        const addInput = this.refs.addInput;
+
+        this.props.addTask(addInput.value); //將value傳入父元件的addTask
+        addInput.value = ''; //input欄位清空
+    }
+}
+
+export default TodoAdd;
